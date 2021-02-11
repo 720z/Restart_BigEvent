68ca304376d0b434ea6f5acd88f70ac8dce3f04d
720z <quin_zhang@163.com>
2021-02-11 11:16:15 +0800

    完成了首页相关功能

--- assets/js/commonAPI.js
+++ assets/js/commonAPI.js
@@ -3,7 +3,11 @@ axios.defaults.baseURL = 'http://api-breakingnews-web.itheima.net';
 
 // 添加请求拦截器(http://www.axios-js.com/zh-cn/docs/#%E6%8B%A6%E6%88%AA%E5%99%A8)
 axios.interceptors.request.use(function(config) {
-    // 在发送请求之前做些什么
+    // 判断接口是否以 /my 开头，设置请求头(注意书写格式⭐)
+    const token = localStorage.getItem('token') || ''
+    if (config.url.startsWith('/my')) {
+        config.headers.Authorization = token
+    }
     return config;
 }, function(error) {
     // 对请求错误做些什么
@@ -12,8 +16,12 @@ axios.interceptors.request.use(function(config) {
 
 // 添加响应拦截器
 axios.interceptors.response.use(function(response) {
+    // 每次发送请求都判断一下是否有token令牌
     const { message, status } = response.data
-        // 对响应数据做点什么
+    if (message == '身份认证失败！' && status == 1) {
+        localStorage.removeItem('token')
+        location.href = './login.html'
+    }
     return response.data;
 }, function(error) {
     // 对响应错误做点什么
---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+// 首页功能 1.封装函数 获取并渲染头像用户名  2.退出按钮
+
+// 1.封装函数 获取并渲染头像用户名
+function getUserInfo() {
+    axios.get('/my/userinfo').then(res => {
+        console.log(res);
+        if (res.status !== 0) return layer.msg('获取失败!')
+
+        //渲染头像与昵称
+        const { user_pic, nickname, username } = res.data
+        const name = nickname || username
+        if (res.data.user_pic) {
+            $('.avatar').prop('src', user_pic).show()
+            $('.text-avatar').hide()
+        } else {
+            $('.text-avatar').text(name[0].toUpperCase()).show()
+            $('.avatar').hide()
+        }
+
+        $('.nickname').text(`欢迎 ${name}`).show()
+    })
+}
+getUserInfo()
+
+// 2.退出按钮
+$('#logout').click(function() {
+    //弹出提示框
+    layer.confirm('确认退出登录？', { icon: 3, title: '提示' }, function(index) {
+        //do something
+        localStorage.removeItem('token')
+        layer.msg('已退出登录!')
+        setTimeout(() => {
+            location.href = './index.html' //延迟跳转首页
+        }, 600);
+
+        layer.close(index);
+    });
+})
